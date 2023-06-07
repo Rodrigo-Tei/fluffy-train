e61a23c7f433ea1518e7095615517a2a5041d1bd
beatriz.neves <beatriznevesporto@hotmail.com>
2023-06-06 22:19:52 -0300

    Caching logged info

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
 import 'package:fluffy_train/login_page.dart';
 import 'package:fluffy_train/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'modules/home/ui/home_page.dart';
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
@@ -10,12 +12,27 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  bool isLogged = false;
+
+  getInitialPage() async {
+    final SharedPreferences prefs = await _prefs;
+
+    setState(() {
+      if (prefs.getBool('logged') != null) {
+        isLogged = prefs.getBool('logged')!;
+      }
+      ;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: DefaultTheme.theme,
-      home: const LoginPage(),
+      home: isLogged ? const HomePage() : const LoginPage(),
     );
   }
 }
--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -3,8 +3,11 @@ import 'package:fluffy_train/modules/home/ui/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
   signInWithGoogle(BuildContext context) async {
     final GoogleSignInAccount? googleUser =
         await GoogleSignIn(scopes: ["email"]).signIn();
@@ -17,13 +20,18 @@ class AuthService {
       idToken: googleAuth.idToken,
     );
 
-    return await FirebaseAuth.instance.signInWithCredential(credential).then(
+    final SharedPreferences prefs = await _prefs;
+
+    return await FirebaseAuth.instance
+        .signInWithCredential(credential)
+        .then(
           (value) => Navigator.of(context).pushReplacement(
             MaterialPageRoute(
               builder: (context) => const HomePage(),
             ),
           ),
-        );
+        )
+        .then((value) => prefs.setBool('logged', true));
   }
 
   signOut(BuildContext context) async {
@@ -32,5 +40,7 @@ class AuthService {
         builder: (context) => const LoginPage(),
       ),
     );
+    final SharedPreferences prefs = await _prefs;
+    prefs.setBool('logged', false);
   }
 }
