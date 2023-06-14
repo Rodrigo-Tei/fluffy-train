3d975d3f2c88f65aff422d3b13d490ec2596b705
Rodrigo-Tei <rodrigoteidalmas@gmail.com>
2023-06-14 20:23:51 -0300

    Fix persist bug

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,6 @@
-import 'package:fluffy_train/login_page.dart';
+import 'package:fluffy_train/auth_service.dart';
 import 'package:fluffy_train/theme/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'modules/home/ui/home_page.dart';
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
@@ -12,27 +10,12 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
-
-  bool isLogged = false;
-
-  getInitialPage() async {
-    final SharedPreferences prefs = await _prefs;
-
-    setState(() {
-      if (prefs.getBool('logged') != null) {
-        isLogged = prefs.getBool('logged')!;
-      }
-      ;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: DefaultTheme.theme,
-      home: isLogged ? const HomePage() : const LoginPage(),
+      home: AuthService().handleAuthState(),
     );
   }
 }
--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -3,10 +3,20 @@ import 'package:fluffy_train/modules/home/ui/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
-  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  handleAuthState() {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.hasData) {
+          return const HomePage();
+        } else {
+          return const LoginPage();
+        }
+      },
+    );
+  }
 
   signInWithGoogle(BuildContext context) async {
     final GoogleSignInAccount? googleUser =
@@ -20,27 +30,21 @@ class AuthService {
       idToken: googleAuth.idToken,
     );
 
-    final SharedPreferences prefs = await _prefs;
-
-    return await FirebaseAuth.instance
-        .signInWithCredential(credential)
-        .then(
+    return await FirebaseAuth.instance.signInWithCredential(credential).then(
           (value) => Navigator.of(context).pushReplacement(
             MaterialPageRoute(
               builder: (context) => const HomePage(),
             ),
           ),
-        )
-        .then((value) => prefs.setBool('logged', true));
+        );
   }
 
   signOut(BuildContext context) async {
+    FirebaseAuth.instance.signOut();
     Navigator.of(context).pushReplacement(
       MaterialPageRoute(
         builder: (context) => const LoginPage(),
       ),
     );
-    final SharedPreferences prefs = await _prefs;
-    prefs.setBool('logged', false);
   }
 }
